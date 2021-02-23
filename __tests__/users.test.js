9e6dc6580cd3144b4366e2f6ccb6b508ac1cba31
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-23 16:28:23 +0800

    Add tests for /users endpoint

---
+++ __tests__/users.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("/users", () => {
+  it("should respond correctly to a POST request", async () => {
+    const response = await request(app)
+      .post("/users")
+      .send({ username: "YOLO!" })
+      .expect(200);
+    expect(response.text).toEqual(
+      "You would like to create a user with username YOLO!"
+    );
+  });
+});
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  res.send(
-    `You would like to create a user with username ${req.body.username}`
-  );
+  res
+    .status(200)
+    .send(`You would like to create a user with username ${req.body.username}`);
 });
 
 module.exports = router;
