0302d211b4fef077915170590ff132cfe900dea4
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-23 14:55:39 +0800

    Write GET, PUT and DELETE tests for /movie endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -111,7 +111,40 @@ describe("/movies", () => {
         movieName: "Lion King",
       },
     ];
-    const response = await request(app).get("/movies").expect(200);
-    expect(response.body).toEqual(expectedResult);
+    const { body: allMovies } = await request(app).get("/movies").expect(200);
+    expect(allMovies).toEqual(expectedResult);
+  });
+
+  it("should respond correctly to a GET request with movie ID", async () => {
+    const expectedResult = {
+      id: 1,
+      movieName: "Lion King",
+    };
+    const { body: actualMovie } = await request(app)
+      .get("/movies/1")
+      .expect(200);
+    expect(actualMovie).toEqual(expectedResult);
+  });
+
+  it("should respond correctly to a PUT request with movie ID", async () => {
+    const expectedResult = {
+      movieName: "Frozen 2",
+    };
+    const { body: updatedMovie } = await request(app)
+      .put("/movies/1")
+      .expect(200);
+    expect(updatedMovie).toMatchObject(expectedResult);
+  });
+  it("should respond correctly to a DELETE request", async () => {
+    const expectedResult = {
+      id: 1,
+      movieName: "Lion King",
+    };
+    const { body: deletedMovie } = await request(app)
+      .delete("/movies/1")
+      .expect(200);
+    expect(deletedMovie).toEqual(expectedResult);
+    const { body: allMovies } = await request(app).get("/movies").expect(200);
+    expect(allMovies.length).toEqual(0);
   });
 });
