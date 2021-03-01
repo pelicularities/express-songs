4d0295a34077bd6ee018098ca121642785157cdd
Grace Teng <pelicularities@users.noreply.github.com>
2021-03-01 11:24:44 +0800

    Remove unneeded testing files

--- test/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const teardownMongoose = async () => {
-  // proper teardown of mongoose to prevent leaks
-  await mongoose.disconnect();
-};
-
-module.exports = { teardownMongoose };
--- test/teardown.js
+++
@@ -1,3 +0,0 @@
-module.exports = async () => {
-  await global.__MONGOSERVER__.stop();
-};
