21436e3ab28f9f9957e71c12f584759064893a4b
Grace Teng <pelicularities@users.noreply.github.com>
2021-03-02 14:17:56 +0800

    Split login/logout routes into separate login router

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-require("dotenv").config();
-require("./utils/db");
-const User = require("./models/user.model");
-const bcrypt = require("bcryptjs");
-const createJWTToken = require("./config/jwt");
+// EXTERNAL IMPORTS
+const express = require("express");
 const cookieParser = require("cookie-parser");
 
-const express = require("express");
+// INTERNAL IMPORTS
+require("dotenv").config();
+require("./utils/db");
+
+// APP SETUP AND EXTERNAL MIDDLEWARE
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-// MIDDLEWARE
+// INTERNAL MIDDLEWARE
 const requireJsonContent = (req, res, next) => {
   if (req.headers["content-type"] !== "application/json") {
     res.status(400).send("Server wants application/json");
@@ -32,51 +33,16 @@ app.post("/", (req, res) => {
   res.status(201).send("Thanks for the JSON!");
 });
 
-// ROUTES - LOGIN / LOGOUT
-app.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const result = await bcrypt.compare(password, user.password);
-
-    if (!result) {
-      throw new Error("Login failed");
-    }
-
-    const token = createJWTToken(user.username);
-
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneWeek = oneDay * 7;
-    const expiryDate = new Date(Date.now() + oneWeek);
-
-    res.cookie("token", token, {
-      // you are setting the cookie here, and the name of your cookie is `token`
-      expires: expiryDate,
-      httpOnly: true, // client-side js cannot access cookie info
-      secure: true, // use HTTPS
-    });
-
-    res.send("You are now logged in!");
-  } catch (err) {
-    if (err.message === "Login failed") {
-      err.statusCode = 400;
-    }
-    next(err);
-  }
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("token").send("You are now logged out!");
-});
-
 // ROUTERS
 const songsRouter = require("./routes/songs.routes");
 const moviesRouter = require("./routes/movies.routes");
 const usersRouter = require("./routes/users.routes");
+const loginRouter = require("./routes/login.routes");
 
 app.use("/songs", songsRouter);
 app.use("/movies", moviesRouter);
 app.use("/users", usersRouter);
+app.use("/", loginRouter);
 
 // ERROR HANDLERS
 app.use((err, req, res, next) => {
---
+++ routes/login.routes.js
@@ -0,0 +1,49 @@
+// EXTERNAL IMPORTS
+const express = require("express");
+const bcrypt = require("bcryptjs");
+
+// INTERNAL IMPORTS
+const createJWTToken = require("../config/jwt");
+const User = require("../models/user.model");
+
+// ROUTER SETUP
+const router = express.Router();
+
+// ROUTES
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const result = await bcrypt.compare(password, user.password);
+
+    if (!result) {
+      throw new Error("Login failed");
+    }
+
+    const token = createJWTToken(user.username);
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneWeek = oneDay * 7;
+    const expiryDate = new Date(Date.now() + oneWeek);
+
+    res.cookie("token", token, {
+      // you are setting the cookie here, and the name of your cookie is `token`
+      expires: expiryDate,
+      httpOnly: true, // client-side js cannot access cookie info
+      secure: true, // use HTTPS
+    });
+
+    res.send("You are now logged in!");
+  } catch (err) {
+    if (err.message === "Login failed") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("token").send("You are now logged out!");
+});
+
+module.exports = router;
